8c36a5123e142f36a801cf2402ad4bb2c3a18cb7
mohamed arshad <mohamedarshadcholasseri5050@gmail.com>
2021-03-15 15:21:12 +0530

    update Setttings

--- grocessory/settings.py
+++ grocessory/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
